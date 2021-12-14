a646d3ddc3e37952c244c3704848ace6088718e1
RemiDrt <rd.duret@hotmail.fr>
2021-12-14 01:31:02 +0100

    copy new views to adapt for darwins

--- docker-web/www/Views/view_begin.php
+++ docker-web/www/Views/view_begin.php
@@ -13,6 +13,8 @@
 				<li><a href="?controller=list&action=last_darwin"> Last Darwin Prizes</a></li>
 				<li><a href="?controller=set&action=form_add_darwin"> Add a Darwin prize</a></li>
 				<!-- TODO : Create action last_darwin and form_add_darwin-->
+				<!-- TODO : Create views form_update_darwin form_add_darwin form_remove_darwin -->
+				<!-- TODO : Create views informations_darwin list_darwin -->
 			</ul>
 		</nav>
 
